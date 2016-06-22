f305e2e0bcc63ecd266ad38959e40b6addef5a07
noli <postmarxed@users.noreply.github.com>
2016-06-21 21:21:54 -0500

    Create tokens.py

---
+++ tokens.py
@@ -0,0 +1,7 @@
+##access tokens from twitter go in the quotes
+##i didn't want to publish these as it would compromise the bots twitter account
+
+C_KEY = " "
+C_SECRET = " "
+A_TOKEN = " "
+A_TOKEN_SECRET = " "
